392144d4d498f17c15e5e7149cccf0aaf73e475e
Lucas Bonan <lucasbonan15@gmail.com>
2024-10-20 23:07:01 -0300

    Adicionando edição nos cards

--- src/components/ContactCard/index.tsx
+++ src/components/ContactCard/index.tsx
@@ -1,27 +1,85 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button, ButtonRemove, ButtonSave } from '../../styles'
 import * as S from './styles'
 import ContatosClass from '../../models/ContatosClass'
 import { useDispatch } from 'react-redux'
-import { remover } from '../../store/reducers/contatos'
+import { remover, editar } from '../../store/reducers/contatos'
 
 type Props = ContatosClass
 
-const ContactCards = ({ titulo, email, telefone, id }: Props) => {
+const ContactCards = ({
+  titulo: tituloOriginal,
+  email: emailOriginal,
+  telefone: telefoneOriginal,
+  id
+}: Props) => {
   const dispatch = useDispatch()
   const [isEditing, setIsEditing] = useState(false)
+  const [titulo, setTitulo] = useState('')
+  const [email, setEmail] = useState('')
+  const [telefone, setTelefone] = useState('')
+
+  useEffect(() => {
+    if (tituloOriginal.length > 0) {
+      setTitulo(tituloOriginal)
+    }
+  }, [tituloOriginal])
+
+  useEffect(() => {
+    if (emailOriginal.length > 0) {
+      setEmail(emailOriginal)
+    }
+  }, [emailOriginal])
+
+  useEffect(() => {
+    if (telefoneOriginal.length > 0) {
+      setTelefone(telefoneOriginal)
+    }
+  }, [telefoneOriginal])
+
+  function cancelarEdicao() {
+    setIsEditing(false)
+    setTitulo(tituloOriginal)
+    setEmail(emailOriginal)
+    setTelefone(telefoneOriginal)
+  }
+
   return (
     <S.Card>
-      <S.Titulo disabled={!isEditing} value={titulo}></S.Titulo>
-      <S.Email disabled={!isEditing} value={email}></S.Email>
-      <S.Telefone disabled={!isEditing} value={telefone}></S.Telefone>
+      <S.Titulo
+        disabled={!isEditing}
+        value={titulo}
+        onChange={(ev) => setTitulo(ev.target.value)}
+      ></S.Titulo>
+      <S.Email
+        disabled={!isEditing}
+        value={email}
+        onChange={(ev) => setEmail(ev.target.value)}
+      ></S.Email>
+      <S.Telefone
+        disabled={!isEditing}
+        value={telefone}
+        onChange={(ev) => setTelefone(ev.target.value)}
+      ></S.Telefone>
       <S.ActionBar>
         {isEditing ? (
           <>
-            <ButtonSave>Salvar</ButtonSave>
-            <ButtonRemove onClick={() => setIsEditing(false)}>
-              Cancelar
-            </ButtonRemove>
+            <ButtonSave
+              onClick={() => {
+                dispatch(
+                  editar({
+                    titulo,
+                    email,
+                    telefone,
+                    id
+                  })
+                )
+                setIsEditing(false)
+              }}
+            >
+              Salvar
+            </ButtonSave>
+            <ButtonRemove onClick={cancelarEdicao}>Cancelar</ButtonRemove>
           </>
         ) : (
           <>
--- src/store/reducers/contatos.ts
+++ src/store/reducers/contatos.ts
@@ -36,9 +36,18 @@ const contatoSlice = createSlice({
       state.itens = state.itens.filter(
         (contato) => contato.id !== action.payload
       )
+    },
+    editar: (state, action: PayloadAction<ContatosClass>) => {
+      const indexDoContato = state.itens.findIndex(
+        (c) => c.id === action.payload.id
+      )
+
+      if (indexDoContato >= 0) {
+        state.itens[indexDoContato] = action.payload
+      }
     }
   }
 })
 
-export const { remover } = contatoSlice.actions
+export const { remover, editar } = contatoSlice.actions
 export default contatoSlice.reducer
